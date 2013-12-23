cd4be6cb4cac98cbc1ab4efa5278db065c957f44
Thomas Brasser <thomasbrasser@gmail.com>
2013-12-23 22:48:07 +0100

    ff tussentijdse commit want yolo
    Ik wil toch de navigatie enzo includen ipv in elke view te doen want zo
    is het alsnog 3dubbel werk. Maar ik maak het wel eerst los in elke view.
    <3

--- app/views/jList.blade.php
+++ app/views/jList.blade.php
@@ -1,8 +1,17 @@
-<?php if (Session::has('message')): ?>
+<?php if(Session::has('message')): ?>
     <?php echo Session::get('message'); ?>
 <?php endif; ?>
-<html>
+
+<!doctype html>
+<html lang="en">
     <head>
+        <meta charset="utf-8" />
+        <title>Wieklikt</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css"></script>
         <script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
@@ -14,28 +23,39 @@
         </style>
     </head>
     <body>
-        <input id="search" type="text" placeholder="Search friends..." />
-        <a href="{{action('ApplicationController@getMatch');}}">Matches</a>
-        <?php $friends = $friendsList['data']; ?>
-        <div id="container" style="margin:0 auto">
-
+        <div class="container">
+            <h1>Klikken maar!</h1>
+            <p>
+                <input id="search" type="text" placeholder="Search friends..." />
+                <a href="{{action('ApplicationController@getMatch');}}">Matches</a>
+            </p>
+            
+            <?php $friends = $friendsList['data']; ?>
+    
             <?php for ($i = 0; $i < 100; $i++): ?>
                 <?php
-                $id = $friends[$i]['id'];
-                $name = $friends[$i]['name'];
+                    $id = $friends[$i]['id'];
+                    $name = $friends[$i]['name'];
                 ?>
-                <div class="profileImage" style="float:left;padding:20px; width:200px">
-                    <a href="/app/click/<?php echo $id ?>">
-                        <img  id="<?php echo $id; ?>" src="https://graph.facebook.com/<?php echo $id; ?>/picture?width=200&height=200 " />
-                    </a>
-                </div>
+                <a href="/app/click/<?php echo $id ?>">
+                    <img    
+                            class="img-rounded" 
+                            src="https://graph.facebook.com/<?php echo $id; ?>/picture?width=200&height=200" 
+                            title="<?php echo $name ?>" 
+                            alt="<?php echo $name ?>" 
+                            width="200px" 
+                    />
+                </a>
             <?php endfor; ?>
-            <div style="clear:both"></div>
         </div>
+
+        <!-- Javascript -->
         <script type="text/javascript">
             //            $(".profileImage").click(function() {
             //                $(this).animate({opacity: "0.0"}).animate({width: 0}).hide(0);
             //            })
         </script>
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
     </body>
 </html>
