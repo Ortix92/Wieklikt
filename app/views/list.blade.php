fe64986ed2e90d4c8e527458ef260f4f1d253aaf
Thomas Brasser <thomasbrasser@gmail.com>
2013-12-23 22:48:07 +0100

    ff tussentijdse commit want yolo
    Ik wil toch de navigatie enzo includen ipv in elke view te doen want zo
    is het alsnog 3dubbel werk. Maar ik maak het wel eerst los in elke view.
    <3

--- app/views/list.blade.php
+++ app/views/list.blade.php
@@ -1,8 +1,18 @@
 <?php if (Session::has('message')): ?>
     <?php echo Session::get('message'); ?>
 <?php endif; ?>
-<html>
+
+<!doctype html>
+<html lang="en">
     <head>
+        <meta charset="utf-8" />
+        <title>Wieklikt</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
+        
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css"></script>
         <script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
@@ -13,23 +23,21 @@
         </style>
     </head>
     <body>
+        <div class="container">
         <?php $friends = $friendsList['data']; ?>
         <div id="container" style="margin:0 auto">
-            <div style="margin:0 auto">
+            <div style="margin:0 auto"> <!-- nick, moet dat id="facebookid" in 't plaatje? Want dat heb je helemaal niet nodig toch? of gebruik je dat in de backend? -->
 
                 <?php for ($i = 0; $i < 100; $i++): ?>
                     <?php
                     $id = $friends[$i]['id'];
                     $name = $friends[$i]['name'];
                     ?>
-                    <div class="profileImage" style="float:left;padding:20px; width:200px">
-                        <a href="/app/click/<?php echo $id ?>">
-                            <img  id="<?php echo $id; ?>" src="https://graph.facebook.com/<?php echo $id; ?>/picture?width=200&height=200 " />
-                        </a>
-                    </div>
+                        <a href="/app/click/<?php echo $id ?>"><img  class="img-rounded" src="https://graph.facebook.com/<?php echo $id; ?>/picture?width=200&height=200" /></a>
                 <?php endfor; ?>
                 <div style="clear:both"></div>
             </div>
+        </div>
         </div>
         <script type="text/javascript">
             //            $(".profileImage").click(function() {
--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -1,35 +1,36 @@
-<?php if(Session::has('message')): ?>
-    <?php echo Session::get('message'); ?>
-<?php endif; ?>
 <!doctype html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8" />
         <title>Wieklikt</title>
         
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Stylesheets -->
         <link rel="stylesheet" href="assets/css/bootstrap.css">
         <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
     </head>
     <body>
+        <?php if(Session::has('message')): ?>
+            <p class="text-danger"><?php echo Session::get('message'); ?></p>
+        <?php endif; ?>
+        
         <div class="container">
         <?php if (!empty($data)): ?>
             <header>
-                <h1>Hello, <?php echo e($data['name']); ?></h1>
+                <h1><small>Hello</small> <?php echo e($data['name']); ?></h1>
             </header>
             <div class="navbar">
                 <ul class="nav navbar-nav">
-                    <li class="active">Home</li>
+                    <li class="active"><a href="/">Home</a></li>
                     <li><a href="app">Naar de App</a></li>
                     <li><a href="logout">Logout</a></li>
                 </ul>
             </div>
             <p>
-                
-                <img src="<?php echo $data['photo']; ?>">
+                <img src="<?php echo $data['photo']; ?>" class="img-responsive img-rounded" alt="userphoto" />
             </p>
             <p>
-                Your email is <?php echo $data['email']; ?>
+                Your email is <a href="mailto:<?php echo $data['email']; ?>"><?php echo $data['email']; ?></a>.
             </p>
             
         <!-- end of logged in -->
