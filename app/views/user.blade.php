0cba7bddea23983e2cc78f5ac3fcfa61f1ba2142
Thomas Brasser <thomasbrasser@gmail.com>
2013-12-23 19:37:38 +0100

    Hoezeee, begonnen aan de frontend
    leukleukleuk

--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -1,15 +1,42 @@
 <?php if(Session::has('message')): ?>
     <?php echo Session::get('message'); ?>
 <?php endif; ?>
-<br>
-<?php if (!empty($data)): ?>
-    Hello, <?php echo e($data['name']); ?>
-    <img src="<?php echo $data['photo']; ?>">
-    <br>
-    Your email is <?php echo $data['email']; ?>
-    <br>
-    <a href="app">Naar de App</a>
-    <a href="logout">Logout</a>
-<?php else: ?>
-    Hi! Would you like to <a href="login/fb">Login with Facebook</a>?
-<?php endif; ?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Wieklikt</title>
+        
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container">
+        <?php if (!empty($data)): ?>
+            
+            <p>
+                Hello, <?php echo e($data['name']); ?>
+                <img src="<?php echo $data['photo']; ?>">
+            </p>
+            <p>
+                Your email is <?php echo $data['email']; ?>
+            </p>
+            <p>
+                <a href="app">Naar de App</a>
+                <a href="logout">Logout</a>
+            </p>
+            
+        <!-- end of logged in -->
+        <?php else: ?>
+            
+            <p>
+                Hi! Would you like to <a href="login/fb">Login with Facebook</a>? 
+            </p>
+            
+        <?php endif; ?>
+        </div>
+    <!-- Javascript -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    </body>
+</html>
