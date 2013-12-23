3a0d6ad7094a94d933575434a254b8dfd59088ca
Thomas Brasser <thomasbrasser@gmail.com>
2013-12-23 23:40:05 +0100

    MENU
    MENUUUUUU

--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -9,47 +9,92 @@
         <link rel="stylesheet" href="assets/css/bootstrap.css">
         <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
     </head>
-    <body>
+    <body style="padding-top: 50px;">
         <?php if(Session::has('message')): ?>
             <p class="text-danger"><?php echo Session::get('message'); ?></p>
         <?php endif; ?>
         
-        <div class="container">
         <?php if (!empty($data)): ?>
+        <!-- Begin navigation for logged-in user -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">WieKlikt</a>
+                </div>
+                
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="/">Home</a></li>
+                        <li><a href="app">Naar de app</a></li>
+                        <li><a href="logout">Logout</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->    
+            </div>
+        </nav>
+        <!-- End navigation for logged-in user --> 
+        
+              
+        <div class="container">
             <header>
                 <h1><small>Hello</small> <?php echo e($data['name']); ?></h1>
             </header>
-            <div class="navbar">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="/">Home</a></li>
-                    <li><a href="app">Naar de App</a></li>
-                    <li><a href="logout">Logout</a></li>
-                </ul>
-            </div>
             <p>
-                <img src="<?php echo $data['photo']; ?>" class="img-responsive img-rounded" alt="userphoto" />
+                <img 
+                    src="<?php echo $data['photo']; ?>" 
+                    class="img-responsive img-rounded" 
+                    alt="userphoto" 
+                />
             </p>
             <p>
                 Your email is <a href="mailto:<?php echo $data['email']; ?>"><?php echo $data['email']; ?></a>.
             </p>
-            
+        </div>
         <!-- end of logged in -->
+        
         <?php else: ?>
+                <!-- Begin navigation for not-logged-in user -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">WieKlikt</a>
+                </div>
+                
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="/">Home</a></li>
+                        <li><a href="login/fb">Login</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->    
+            </div>
+        </nav>
+        <!-- End navigation for not-logged-in user --> 
+
+        <div class="container">
             <header>
                 <h1>Hello there,</h1>
             </header>
-            <div class="navbar">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="/">Home</a></li>
-                    <li> <a href="login/fb">Login</a></li>
-                </ul>
-            </div>
             <p>
                 Would you like to <a href="login/fb">Login with Facebook</a>? 
             </p>
-            
-        <?php endif; ?>
         </div>
+        <?php endif; ?>
+
     <!-- Javascript -->
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="assets/js/bootstrap.js"></script>
