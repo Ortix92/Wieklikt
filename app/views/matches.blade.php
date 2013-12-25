4141f93cb12b7c94b826c32dbcb8b303869fede2
Ortix92 <nicktsutsunava@gmail.com>
2013-12-25 14:36:59 +0100

    Fixed menu issues

--- app/views/matches.blade.php
+++ app/views/matches.blade.php
@@ -43,8 +43,8 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li><a href="../">Home</a></li>
-                        <li><a href="../app">Terug naar de app</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/app">Terug naar de app</a></li>
                         <li class="active"><a href="match">Matches</a></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
