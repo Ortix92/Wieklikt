0a3685af967a06aaad82faef80edbace8e5c880f
Nick <nicktsutsunava@gmail.com>
2013-12-26 01:04:11 +0100

    Fixed navigation
    git issues

--- app/config/database.php
+++ app/config/database.php
@@ -54,9 +54,9 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => 'dev.wieklikt.nl',
 			'database'  => 'wieklikt',
-			'username'  => 'wieklikt',
+			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
