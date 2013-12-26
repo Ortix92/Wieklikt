a69f6bd7fdd8a0ac0da23fabbaca1e38a57784f9
Thomas Brasser <thomasbrasser@gmail.com>
2013-12-26 15:08:47 +0100

    autocomplete UI aangepast
    jeuj

--- public/assets/js/wieklikt.js
+++ public/assets/js/wieklikt.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     function display(friends) {
         $(".row").empty();
 
-        $.each(friends, function(index, friend) {
-            var html = '<div class="profileImage" style="float:left;padding:20px; width:200px">';
+        $.each(friends, function (index, friend) {
+            var html = '<div class="col-xs-6 col-sm-4 col-md-2 fb-friend">';
             html += '<a href="/app/click/' + friend.id + '">';
-            html += '<img  id="' + friend.id + ' " src="https://graph.facebook.com/' + friend.id + '/picture?width=200&height=200 " />';
+            html += '<img class="img-rounded img-responsive" width="200px" src="https://graph.facebook.com/' + friend.id + '/picture?width=200&height=200 " title="' + friend.name + ' " alt="' + friend.name + ' " />';
+            html += '<p class="text-center">' + friend.name + ' </p>';
             html += '</a>';
             html += '</div>';
             $(".row").append(html);
@@ -15,13 +16,12 @@ $(document).ready(function() {
     $("#search").autocomplete({
         source: "/app/friends",
         minLength: 2,
-        response: function(event, ui) {
+        response: function (event, ui) {
             display(ui.content);
         },
-        open: function(event, ui) {
+        open: function (event, ui) {
             $(".ui-autocomplete").hide();
         }
     });
 
 });
-
