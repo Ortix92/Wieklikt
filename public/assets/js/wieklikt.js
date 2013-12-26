4620d374391a4fbc800d95f03c1275824c37644a
Nick <nicktsutsunava@gmail.com>
2013-12-26 15:19:35 +0100

    Optimized friend searching

--- public/assets/js/wieklikt.js
+++ public/assets/js/wieklikt.js
@@ -1,25 +1,23 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
     function display(friends) {
         $(".row").empty();
-
-        $.each(friends, function (index, friend) {
-            var html = '<div class="col-xs-6 col-sm-4 col-md-2 fb-friend">';
-            html += '<a href="/app/click/' + friend.id + '">';
-            html += '<img class="img-rounded img-responsive" width="200px" src="https://graph.facebook.com/' + friend.id + '/picture?width=200&height=200 " title="' + friend.name + ' " alt="' + friend.name + ' " />';
-            html += '<p class="text-center">' + friend.name + ' </p>';
-            html += '</a>';
-            html += '</div>';
-            $(".row").append(html);
-        });
+        $(".row").append(friends);
     }
-    
+
     $("#search").autocomplete({
-        source: "/app/friends",
-        minLength: 2,
-        response: function (event, ui) {
-            display(ui.content);
+        source: function(request, response) {
+            $.ajax({
+                url: "app/friends",
+                data: {term: request.term},
+                success: function(data) {
+                    display(data);
+                    console.log(response);
+                }
+            });
         },
-        open: function (event, ui) {
+        minLength: 2,
+        open: function(event, ui) {
             $(".ui-autocomplete").hide();
         }
     });
