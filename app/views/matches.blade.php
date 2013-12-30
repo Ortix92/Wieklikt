8ffba3a6afb067532ebd1a557daeb4490c32c775
Thomas Brasser <thomasbrasser@gmail.com>
2013-12-30 06:55:27 +0100

    Matches page updated and:
    - Menu aangepast
    - footer 1  woord veranderd

--- app/views/matches.blade.php
+++ app/views/matches.blade.php
@@ -1,36 +1,68 @@
 @extends('layouts.base')
 
 @section('navigation-main')
-    <li><a href="/">Home</a></li>
-    <li><a href="/app">Terug naar de app</a></li>
+    <li><a href="/">Profile</a></li>
+    <li><a href="/app">Click</a></li>
     <li class="active"><a href="match">Matches</a></li>
 @stop
 
 @section('main-body')
-    <?php if (!empty($clicks)): ?>
-    <p>
+<?php if (!empty($clicks)): ?>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="page-header">
+            <h1>Woehoehoe</h1>
+        </div>
+    </div>   
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <div class="progress progress-striped active">
           <div class="progress-bar"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
             <span class="sr-only">100% Complete</span>
           </div>
         </div>
-    </p>
-    <h1>Woehoehoe</h1>
-    <?php foreach ($clicks as $click): ?>
-    <a href="http://facebook.com/<?php echo $click["clicker"]; ?>"><img class="img-circle" id="<?php echo $click["clicker"]; ?>" src="https://graph.facebook.com/<?php echo $click["clicker"]; ?>/picture?width=200&height=200 " /></a>           
-    <?php endforeach; ?>
-    <?php else: ?><!-- Geen matches -->
-    <p>
-        <div class="progress progress-striped active">
-          <div class="progress-bar"  role="progressbar" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100" style="width: 66%">
-            <span class="sr-only">66% Complete</span>
-          </div>
+    </div>   
+</div>
+<div class="row">    
+  <?php foreach ($clicks as $click): ?>
+    <div class="col-xs-6 col-sm-4 col-md-2 fb-friend">
+        <a href="http://facebook.com/<?php echo $click["clicker"]; ?>">
+            <img    class="img-circle img-responsive" 
+                    src="https://graph.facebook.com/<?php echo $click["clicker"]; ?>/picture?width=200&height=200 "
+                    title="name"  
+                    alt="name"
+                    width="200px" />
+        </a> 
+    </div>          
+  <?php endforeach; ?>
+</div>
+<?php else: ?><!-- Geen matches -->
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="page-header">
+            <h1>Jammer joh!</h1>
         </div>
-    </p>
-    <h1>Jammer joh!</h1>
-    <p>
-        je hebt nog geen matches
-    </p>
-    <?php endif; ?>
+    </div>   
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="page-header">
+            <div class="progress progress-striped active">
+              <div class="progress-bar"  role="progressbar" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100" style="width: 66%">
+                <span class="sr-only">66% Complete</span>
+              </div>
+            </div>
+        </div>
+    </div>   
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="page-header">
+            <p>je hebt nog geen matches</p>
+        </div>
+    </div>   
+</div>
+<?php endif; ?>
     <div>{{@Session::get("clicked")}}</div>
 @stop
