711535b8f459dedfa3e27ab5a535e1c9b4baf592
Thomas Brasser <thomasbrasser@gmail.com>
2014-05-10 22:36:41 +0200

    fancy clicks pagina
    wollah

--- app/views/clicks.blade.php
+++ app/views/clicks.blade.php
@@ -8,14 +8,28 @@
 @stop
 
 @section('main-body')
-    <h1>Clicks</h1>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="page-header">
+            <h1>Your clicks!</h1>
+        </div>
+    </div>   
+</div>
+<div class="row destacados">
     <?php if (!empty($clicks)): ?><!-- wel clicks -->
-    <p>Your clicks:</p>
     <?php foreach ($clicks as $click): ?>
-    <a href="http://facebook.com/<?php echo $click["clickee"]; ?>"><img class="img-circle" id="<?php echo $click["clickee"]; ?>" src="https://graph.facebook.com/<?php echo $click["clickee"]; ?>/picture?width=200&height=200 " /></a>           
+
+    <div class="col-md-4">
+            <div>
+                <img class="img-circle img-thumbnail" id="<?php echo $click["clickee"]; ?>" src="https://graph.facebook.com/<?php echo $click["clickee"]; ?>/picture?width=200&height=200 " />
+                <h2>Voornaam Achternaam</h2>
+                <a href="#" class="btn btn-primary" title="Enlace">Edit</a>
+            </div>
+        </div>        
     <?php endforeach; ?>
     <?php else: ?><!-- Geen clicks -->
     <p>You haven't clicked anyone yet! This means you still have 3 clicks remaining!</p>
     <?php endif; ?>
     <div>{{@Session::get("clicked")}}</div>
+    </div>
 @stop
