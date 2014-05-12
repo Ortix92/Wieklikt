fcee7aafe042ca9553ca51a3c8589ef61965c5dd
Ortix92 <nicktsutsunava@gmail.com>
2014-05-12 17:07:27 +0200

    Added name in clicks page
    Updated Thomas' revision

--- app/views/clicks.blade.php
+++ app/views/clicks.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.base')
 
 @section('navigation-main')
-    <li><a href="/"><i class="fa fa-user fa-lg fa-fw"></i> Profile</a></li>
-    <li><a href="/app"><i class="fa fa-users fa-lg fa-fw"></i> Click</a></li>
-    <li class="active"><a href="{{action('ApplicationController@getClicks');}}"><i class="fa fa-thumb-tack fa-lg fa-fw"></i> Clicks</a></li>
-    <li><a href="{{action('ApplicationController@getMatch');}}"><i class="fa fa-link fa-lg fa-fw"></i> Matches</a></li>
-    
+<li><a href="/"><i class="fa fa-user fa-lg fa-fw"></i> Profile</a></li>
+<li><a href="/app"><i class="fa fa-users fa-lg fa-fw"></i> Click</a></li>
+<li class="active"><a href="{{action('ApplicationController@getClicks');}}"><i class="fa fa-thumb-tack fa-lg fa-fw"></i> Clicks</a></li>
+<li><a href="{{action('ApplicationController@getMatch');}}"><i class="fa fa-link fa-lg fa-fw"></i> Matches</a></li>
+
 @stop
 
 @section('main-body')
@@ -18,19 +18,21 @@
 </div>
 <div class="row destacados">
 
-<?php foreach((array)$clicks as $i=>$click) { ?>
-       <div class="col-md-4">
+    <?php foreach ((array) $clicks as $i => $click) { ?>
+        <div class="col-md-4">
             <div>
                 <img class="img-circle img-thumbnail" id="<?php echo $click["id"]; ?>" src="https://graph.facebook.com/<?php echo $click["id"]; ?>/picture?width=200&height=200 " />
-                <h2>Voornaam Achternaam {{$i + 1}}</h2>
+                <h2>{{$click['name']}}</h2>
                 <a href="#" class="btn btn-primary" title="Enlace">Edit</a>
             </div>
         </div>     
-   <?php } 
-   $allowedClicks = 9;
-   for ($i = count((array)$clicks); $i < $allowedClicks ; $i++) { ?>
-  
-       <div class="col-md-4">
+    <?php
+    }
+    $allowedClicks = 25;
+    for ($i = count((array) $clicks); $i < $allowedClicks; $i++) {
+        ?>
+
+        <div class="col-md-4">
             <div>
                 <img class="img-circle img-thumbnail" id="emptyclick" src="nogniks" />
                 <h2>LEEG {{$i + 1}}</h2>
@@ -39,8 +41,5 @@
         </div> 
 <?php } ?>
 
-
-
-    <div>{{@Session::get("clicked")}}</div>
-    </div>
+</div>
 @stop
