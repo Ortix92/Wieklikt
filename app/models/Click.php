2ed4c642c8a3ccd91d9eb9fa55fc5a38794bfc45
Ortix92 <nicktsutsunava@gmail.com>
2014-05-16 20:20:00 +0200

    added Query scope

--- app/models/Click.php
+++ app/models/Click.php
@@ -8,4 +8,9 @@ class Click extends Eloquent {
         return $this->belongsTo('Profile', 'clicker', 'uid');
     }
 
+    // Click::clickee($uid);
+    public function scopeClickee($query, $uid) {
+        return $query->where('clickee', '=', $uid);
+    }
+    
 }
